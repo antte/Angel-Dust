a74ea10f0e37cd9db971f0d5d176101e09ac1cfe
Chux <lechux@gmail.com>
2009-09-19 20:03:34 +0200

    Chang so the world the angel spawns in is the one in landscape.lua, world_layer0

--- angel.lua
+++ angel.lua
@@ -6,7 +6,7 @@ function angelLoad()
 	-- Should "world" be an argument in the function?
 
 	-- Create box2d shape and body for the character
-	characterBody = love.physics.newBody(world, 400, 200)
+	characterBody = love.physics.newBody(world_layer0, 400, 200)
 	characterTorsoShape = love.physics.newRectangleShape(characterBody, 100,190, 20,20)
 	characterBody:setMassFromShapes( );
 	characterBody:setAngularDamping(10000)
