019e1f6d46c8ed63837945b5735f550ca5c6bb5b
antte <antte@Athena.(none)>
2009-09-20 16:05:02 +0200

    only checkVelocity(c) for now

---
+++ collision.lua
@@ -0,0 +1,19 @@
+--Sprint 1: Collition detection w/ velocity check
+--author: antte
+--två objekt möts, trace:a med vilken kraft de slår mot varandra
+
+function collisionLoad()
+	
+	world_layer0:setCallback(collision)
+	
+end
+
+function checkVelocity(c)
+	--[[
+	Tar in en collision och skriver ut velocity i debuggen.
+	]]--
+	
+	local vx, vy = c:getVelocity()
+	debugtext = "Velocity: " .. vx .. "," .. vy .. "\n" 
+	
+end
